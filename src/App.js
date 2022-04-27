a8ad6fbc5ab0f6f5ada3cef24522605108b349cb
Lucas-de-Souza-Pinto <lucasdesouzapinto2000@gmail.com>
2022-04-27 02:19:43 -0300

    completed the first level of the challenge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Carousel from "nuka-carousel";
 /* icons */
-import passArrow from "./assets/icons/pass-arrow.svg"
 import IconSearch from "./assets/icons/icon-search-filled.svg"
 import DefaultUserProfile from "./assets/icons/default-user-profile.svg"
 import RightTriangle from "./assets/icons/right-triangle.svg"
@@ -33,13 +32,12 @@ body{
 `
 const ContainerPag =styled.div`
   width:100%;
-  padding:1.1% 0 3.1% 3.9% ;
+  padding:1.1% 0 3.1% 3.9vw ;
   background-color:black;
 `
 const Header = styled.header`
   width:100%;
   height:4.8vh;
-  margin-bottom:7.5vw;
   display:flex;
   background-color:#000;
 `
@@ -53,29 +51,35 @@ const HeaderDiv = styled.div`
   }
 `
 const TODOFLIX = styled.h1`
-  font-size:1.5em;
+  font-size:2.01em;
   color:#E71B27;
   font-family: 'Bebas Neue', cursive;
   
 `
 const Home = styled.p`
   margin:0 1.8vw;
+  font-size:0.8125em;
 
 `
 const AddMovies = styled.button`
   width:112px;
   height:32px;
   border:none;
-  margin:0 1.02vw;
+  margin-right:1.02vw;
   border-radius:4px;
   display:flex;
   justify-content:center;
   align-items:center;
   font: normal normal normal 13px/15px Arial;
   background-color:#E71B27;
+  &:hover{
+    transition:1s;
+    border:1px solid white;
+  }
 `
 const DropDown = styled.div`
   width:132px;
+    cursor: pointer;
   &:hover ul{
     display:block;
   }
@@ -88,7 +92,7 @@ const SummaryCategory = styled.div`
 const SummaryTitle = styled.h3`
   display:flex;
   align-items:center;
-  font-size:0.98em;
+  font-size:0.8125em;
   font-weight:normal;
   `
 const ContentList = styled.ul`
@@ -101,6 +105,9 @@ const ContentList = styled.ul`
     li{
       font-size:0.95em;
       padding:6px;
+      &:hover{
+        text-decoration:underline;
+      }
     }
   `
 const Search =styled.input`
@@ -117,14 +124,13 @@ const Search =styled.input`
 const ConfigUSerProfile = styled.div`
   display:flex;
   align-items:center;
-  position: absolute;
-  right:0;
+  margin-left:1.75vw;
 
 `
-
 const Main = styled.main`
   width:100%;
   height:100%;
+  margin-top:7.5vw;
 
 
 `
@@ -149,6 +155,7 @@ const InfoMovie= styled.div`
   margin-left:3.5vw;
 ` 
 const TitleRecentlySeen  = styled.div`
+  margin-bottom:0.2em;
   font-size:2.55em;
   font-weight:bold;
 `
@@ -156,7 +163,6 @@ const Description = styled.div`
   width:29.648609077vw;
   font-size:0.9em;
 `
-
 const Highlighted= styled.div`
   width:100%;
 ` 
@@ -167,7 +173,7 @@ const SpotlightTitle = styled.h2`
 const MovieBox = styled.div`
   width:18.9vw;
   min-height:20.7vw;
-  padding:2.18%;
+  padding:0.5vw;
   margin:1.5% 0;
   border-radius:4px;
   &:hover{
@@ -186,6 +192,7 @@ const NameAndLike= styled.div`
   justify-content:space-between;
 ` 
 const TitleInMovieBox= styled.h3`
+  width:70%;
   display: inline-block;
   position: relative;
   font-size:1em;
@@ -214,8 +221,7 @@ const Arrow = styled.svg`
     background-color:rgba(255,255,255,0.2);
     box-shadow:0 0 100px -3px rgba(255,255,255,0.2);
   }
-`;
-
+`
 const Figure= styled.figure`
   position:relative;
 `
@@ -224,7 +230,25 @@ const FavoriteHeartButton = styled.img`
   position:absolute;
   right:1.5%;
   top:1.5%;
+
 `
+const ResultsMovies = styled.div`
+  width:100%;
+  padding-right:3.9vw;
+  display:flex;
+  flex-wrap:wrap;
+`
+const CategoryTitle = styled.div`
+  font-size:1.8125em;
+`
+const ResultsBox = styled.div`
+  margin-top:5vh;
+`
+const FavoriteHeartButtonRecentlySeen = styled.img`
+  width:21px;
+  margin-bottom:4px;
+`
+
 // const i = styled.div``
 export default class App extends React.Component{
   state={
@@ -236,15 +260,16 @@ export default class App extends React.Component{
       {poster:Movie2,Name:"AmarElo", evaluationInLikes:"5/5", description:"Nos bastidores do show no Theatro Municipal de São Paulo, o rapper e ativista Emicida celebra o grande legado da cultura negra brasileira.", },
       {poster:Movie3,Name:"Capitão Fantástico", evaluationInLikes:"4/5", description:"Nas florestas do estado de Washington, um pai cria seus seis filhos longe da civilização, em uma rígida rotina de aventuras. Ele é forçado a deixar o isolamento e leva sua família para encarar o mundo, desafiando sua ideia do que significa ser pai.", },
       {poster:Movie4,Name:"Hoje Eu Quero Voltar Sozinho", evaluationInLikes:"-", description:"Um novo colega de sala transforma a vida do estudante Leonardo, que também é cego, e complica sua amizade com sua melhor amiga.", },
-      {poster:Movie5,Name:"Que Horas Ela Volta", evaluationInLikes:"-", description:"Val é a fiel empregada domestica de uma família rica. Mas a chegada de sua filha gera tensão na casa e faz com que ela comece a questionar esse papel.", },
+      {poster:Movie5,Name:"Que Horas Ela Volta?", evaluationInLikes:"-", description:"Val é a fiel empregada domestica de uma família rica. Mas a chegada de sua filha gera tensão na casa e faz com que ela comece a questionar esse papel.", },
       {poster:Movie6,Name:"Rocketman", evaluationInLikes:"-", description:"Em reabilitação, Elton John relembra suas origens humildes, as músicas atemporais e os momentos de inspiração e excesso. Baseado em sua verdadeira história.", },
       {poster:Movie7,Name:"Sherek", evaluationInLikes:"4/5", description:"Para resgatar uma princesa das garras de um dragão que cospe fogo, o ogro Shrek se une a um companheiro improvável: um burro brincalhão.", },
       {poster:Movie8,Name:"Homem Aranha", evaluationInLikes:"3/5", description:"Após ser picado por uma aranha radioativa, o garoto Miles Morales logo aprende a lançar teias com seus parceiros de um universo alternativo.", },
       {poster:Movie9,Name:"Um Sonho de Liberdade", evaluationInLikes:"4,5/5", description:"Condenado pelo assassinato da esposa e do amante dela, um banqueiro se apega à esperança e à amizade com um detento chamado Red para sobreviver à prisão.", },
     ],
     resultMovies:[],
+    displayResults:true,
+    inputValue:"",
   }
-  
   SearchMovies = (event) =>{
     const {moviesList} = this.state
     const MoviesFilter = moviesList.filter(item => {
@@ -254,7 +279,10 @@ export default class App extends React.Component{
     })
 
     this.setState({
-      resultMovies:MoviesFilter
+      resultMovies:MoviesFilter,
+      displayResults: !this.state.displayResults,
+      inputValue:event.target.value
+
     })
   }
   
@@ -281,38 +309,47 @@ export default class App extends React.Component{
           </HeaderDiv>
           <HeaderDiv >
             <AddMovies>Adicionar filme</AddMovies>
+            
               <Search onChange={this.SearchMovies} type="text" placeholder="Pesquisar"/>
             <ConfigUSerProfile>
               <img src={DefaultUserProfile} alt=""/><img src={RightTriangle}/>
             </ConfigUSerProfile>
           </HeaderDiv>
         </Header>
-        <div>
-          <h3>Todos</h3>
-        {this.state.resultMovies.map(item=>(
-                <MovieBox>
-                  <Figure>
-                    <PosterInMovieBox src={item.poster} alt=""/>
-                    <FavoriteHeartButton src={FavoriteHeart} alt="Botão de coração para favoritar."/>
-                  </Figure>
-                  <NameAndLike>
-                  <TitleInMovieBox>{item.Name}</TitleInMovieBox>
-                  <Assessment>
-                    <NoteNumber>{item.evaluationInLikes}</NoteNumber>
-                    <img src={LikeButton} alt="botão com desenho de uma mão com polegar pra cima no estilo minimalista em verde claro."/>
-                  </Assessment>
-                  </NameAndLike>
-                  <DescriptionMovieBox>{item.description}</DescriptionMovieBox>
-                  
-                </MovieBox>
-              ))}
-        </div>
+
+        {this.state.inputValue != ""?
+       <ResultsBox>
+      <CategoryTitle>Results</CategoryTitle>
+      <ResultsMovies>
+      {this.state.resultMovies.map(item=>(
+       <MovieBox>
+         <Figure>
+           <PosterInMovieBox src={item.poster} alt=""/>
+           <FavoriteHeartButton title="Favoritar" src={FavoriteHeart} alt="Botão de coração para favoritar."/>
+         </Figure>
+         <NameAndLike>
+         <TitleInMovieBox>{item.Name}</TitleInMovieBox>
+         <Assessment>
+           <NoteNumber>{item.evaluationInLikes}</NoteNumber>
+           <img src={LikeButton} alt="botão de coração simbolisando favorito"/>
+         </Assessment>
+         </NameAndLike>
+         <DescriptionMovieBox>{item.description}</DescriptionMovieBox>
+         
+       </MovieBox>
+     ))}
+    </ResultsMovies>
+      </ResultsBox> : false}
+  
+       
+
+      {this.state.inputValue == ""?
         <Main>
           <RecentlySeen>
             <FeatureMoviePoster src={this.state.RecentlySeen[0].poster} alt=""/>
 
             <InfoMovie>
-            <img src={FavoriteHeart} alt="Botão de coração para favoritar."/>
+            <FavoriteHeartButtonRecentlySeen title="Favoritar" src={FavoriteHeart} alt="Botão de coração para favoritar."/>
             <p>Visto recentemente</p>
             <TitleRecentlySeen>{this.state.RecentlySeen[0].Name}</TitleRecentlySeen>
             <Description>{this.state.RecentlySeen[0].description}</Description>
@@ -364,12 +401,14 @@ export default class App extends React.Component{
   pagingDotsStyle: {
     fill: 'none'
   }
-}} adaptiveHeight="false" slidesToShow={5} slidesToScroll={4} scrollMode="page">
+            }} adaptiveHeight="false" slidesToShow={5} slidesToScroll={4} scrollMode="page"
+            >
+
               {this.state.moviesList.map(item=>(
                 <MovieBox>
                   <Figure>
                     <PosterInMovieBox src={item.poster} alt=""/>
-                    <FavoriteHeartButton src={FavoriteHeart} alt="Botão de coração para favoritar."/>
+                    <FavoriteHeartButton title="Favoritar" src={FavoriteHeart} alt="Botão de coração para favoritar."/>
                   </Figure>
                   <NameAndLike>
                   <TitleInMovieBox>{item.Name}</TitleInMovieBox>
@@ -384,7 +423,7 @@ export default class App extends React.Component{
               ))}
             </Carousel>
           </Highlighted>
-        </Main>
+        </Main>: false}
         
       </ContainerPag>
     )
